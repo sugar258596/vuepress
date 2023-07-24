70f37b46352768e40034da5f0b1b2975aec496d9
sugar258596 <84883966+sugar258596@users.noreply.github.com>
2023-07-24 16:46:58 +0800

    Deploying to gh-pages from  @ dd751aebef272447405f47069410b17781a97a21 🚀

--- assets/deployment.html-4cc8e2bb.js
+++ assets/deployment.html-4cc8e2bb.js
@@ -1,7 +1,7 @@
 import{_ as s,p as a,q as e,a1 as n}from"./framework-d81ad7e5.js";const i={},l=n(`<h1 id="部署" tabindex="-1"><a class="header-anchor" href="#部署" aria-hidden="true">#</a> 部署</h1><h2 id="部署方式" tabindex="-1"><a class="header-anchor" href="#部署方式" aria-hidden="true">#</a> 部署方式</h2><p>两种部署方式</p><ol><li><p>github actions</p><ul><li>将 <code>.github/workflows/ci.yml</code> 文件注释打开</li><li>检查配置，默认部署分支是 <code>gh-pages</code></li></ul></li><li><p>push 打包后的静态文件夹 检查配置,默认配置如下</p></li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment"># 项目发布域名</span>
-<span class="token assign-left variable">web_url</span><span class="token operator">=</span>https://sugar258596.github.io/vuepress2/
+<span class="token assign-left variable">web_url</span><span class="token operator">=</span>https://sugar258596.github.io/vuepress/
 <span class="token comment"># 项目git 地址</span>
-<span class="token assign-left variable">git_source</span><span class="token operator">=</span>https://github.com/sugar258596/vuepress2
+<span class="token assign-left variable">git_source</span><span class="token operator">=</span>https://github.com/sugar258596/vuepress
 <span class="token comment"># 上传分支，如果是 master 请使用 main_branch=master</span>
 <span class="token assign-left variable">main_branch</span><span class="token operator">=</span>main
 <span class="token comment"># 项目打包生成的上传分支</span>
